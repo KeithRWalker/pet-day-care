8206162cb80f961374c85cf4504913a622c15486
Keith Walker <Keithrwalker418@gmail.com>
2019-06-27 20:02:11 -0500

    login/logout working

--- src/App/App.js
+++ src/App/App.js
@@ -1,33 +1,64 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import myPets from './pets';
 import PetPen from '../components/PetPen/PetPen';
 import myStaff from './staff';
-import StaffRoom from '../components/StaffRoom/StaffRoom';
+import MyNavbar from '../components/MyNav/MyNav';
+// import StaffRoom from '../components/StaffRoom/StaffRoom';
+
+import Auth from '../components/Auth/Auth';
+// import Home from '../components/Home/Home';
 
 import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import fbConnection from '../helpers/data/connection';
+
+fbConnection();
 
 class App extends React.Component {
   state = {
+    authed: false,
     pets: [],
     staff: [],
   }
 
   componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
     this.setState({ pets: myPets });
     this.setState({ staff: myStaff });
   }
 
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
     const { pets } = this.state;
-    const { staff } = this.state;
+    // const { staff } = this.state;
+    const loadComponent = () => {
+      if (authed) {
+        return <PetPen pets={pets}/>;
+      }
+      return <Auth />;
+    };
+
     return (
       <div className="App">
-        <div className="row main-con">
-          <PetPen pets={pets}/>
-          <StaffRoom staff={staff}/>
-        </div>
+        {/* <div className="row main-con"> */}
+        <MyNavbar authed={authed} />
+        {loadComponent()}
+        {/* <StaffRoom staff={staff}/> */}
+        {/* </div> */}
       </div>
     );
   }
